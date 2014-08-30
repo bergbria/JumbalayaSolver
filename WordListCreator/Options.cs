0a7b56dd3963411e00b710122eda67aff30c0e05
Brian Berger <bergbria@gmail.com>
2014-08-30 13:14:25 -0700

    stubbed out initial projects, added wordlist, and implemented a project to filter down a wordlist to only valid Jumbalaya words

---
+++ WordListCreator/Options.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace WordListCreator
+{
+    class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Path to the word list to process and filter", MetaValue = "<file path>")]
+        public string InputFile { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Where to put the processed file", MetaValue = "<file path>")]
+        public string OutputFile { get; set; }
+    }
+}
---
+++ WordListCreator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using CommandLine.Text;
+
+namespace WordListCreator
+{
+    class Program
+    {
+        //don't allow any spaces, punctuation, or uppercase words
+        private static readonly Regex ValidWordLineRegex = new Regex(@"^[a-z]+$");
+
+        //Usage <exe name> <input word list> <destination file>
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Console.WriteLine(HelpText.AutoBuild(options));
+            }
+
+            if (!File.Exists(options.InputFile))
+            {
+                Console.WriteLine("Could not find input file");
+            }
+
+
+            using (var outputStream = new StreamWriter(options.OutputFile, false))
+            {
+                foreach (var line in File.ReadLines(options.InputFile))
+                {
+                    if (ValidWordLineRegex.IsMatch(line))
+                    {
+                        outputStream.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
